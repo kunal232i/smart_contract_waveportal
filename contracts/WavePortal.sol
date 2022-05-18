00f8c2482aae77a144bdeb5deaf843ee4ece4361
kunal232i <kingawale23@gmail.com>
2022-05-18 00:31:08 +0000

    Update

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -1,13 +1,10 @@
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
-
-    /*
-     * We will be using this below to help generate a random number
-     */
     uint256 private seed;
 
     event NewWave(address indexed from, uint256 timestamp, string message,uint256 ethWon, bool);
@@ -22,9 +19,6 @@ contract WavePortal {
     mapping(address => uint256) public lastWavedAt;
     constructor() payable {
         console.log("We have been constructed!");
-        /*
-         * Set the initial seed
-         */
         seed = (block.timestamp + block.difficulty) % 100;
     }
 
@@ -33,33 +27,17 @@ contract WavePortal {
             lastWavedAt[msg.sender] + 30 seconds < block.timestamp,
             "Must wait 30 seconds before waving again."
         );
-
-        /*
-         * Update the current timestamp we have for the user
-         */
         lastWavedAt[msg.sender] = block.timestamp;
 
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
 
         waves.push(Wave(msg.sender, _message, block.timestamp));
-
-        /*
-         * Generate a new seed for the next user that sends a wave
-         */
         seed = (block.difficulty + block.timestamp + seed) % 100;
 
         console.log("Random # generated: %d", seed);
-
-        /*
-         * Give a 50% chance that the user wins the prize.
-         */
         if (seed <= 50) {
             console.log("%s won!", msg.sender);
-
-            /*
-             * The same code we had before to send the prize.
-             */
             uint256 prizeAmount = 0.0001 ether;
             require(
                 prizeAmount <= address(this).balance,
